7711e238b346cc28ebbbeb623b45ec3dcefc31a4
yanshinpeng <highbess@gmail.com>
2020-03-30 20:30:18 +0800

    Initial Commit

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,19 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by cycu on 2020/3/30.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData:ObservableObject{
+    @Published var showFavoriteOnl=false
+    @Published var landmarks = landmarkData
+}
+
+
+
+ 
